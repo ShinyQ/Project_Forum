56b73eac4449c3e4902485cfe065676b17dd69d1
Kurniadi Ahmad Wijaya <kurniadiahmadwijaya@gmail.com>
2017-06-26 19:46:30 +0700

    backup

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+	<title>Test Forum</title>
+	<meta name="viewport" content="width=device-width initial-scale=1 maximum-scale=1">
+</head>
+
+<body> 
+
+<header>
+	<span>Test Forum</span>
+</header>
+
+<h1>Kurniadi Ahmad Wijaya</h1>
+<h1>Ahmad Hasib Haidar</h1>
+<h1>Diva Mujaddidi</h1>
+
+<h1> Hello World </h1>
+
+</body>
+</html>
