b12da58665456c94e152b5cf1eb8f5b48bcd0df1
Kurniadi Ahmad Wijaya <kurniadiahmadwijaya@gmail.com>
2017-06-26 20:07:49 +0700

    Rapikan Text

--- index.php
+++ index.php
@@ -13,27 +13,17 @@
 <strong>&nbsp;&nbsp;<!-- Untuk Image logo --></strong>
 <br>
 
-<form class="form" style="float:right;">
-
-</form>
 <strong>
 <div style="float:center,padding-bottom:10px;">
 			
 			
-			<font size="6px" COLOR="#e52b2b">&nbsp; <!-- nama web --> </font>
-			<i>
-			<font color="#96b5e8" size="4px"><br>&nbsp;&nbsp;&nbsp;&nbsp;
-			<!-- slogan -->
-			</font>
-			</strong></i>
-
-</div>
-</div>
+<font size="6px" COLOR="#e52b2b">&nbsp; <!-- nama web --> </font>
+<i><font color="#96b5e8" size="4px"><br>&nbsp;&nbsp;&nbsp;&nbsp;<!-- slogan --></font>
+</strong></i></div></div>
+	
 <div class="navbar">
 <ul>
-<li>  
-<strong><span style="font-size:20px;cursor:pointer" onclick="openNav()">&#9776; Profil Anda</span></strong>
-</li>
+<li><strong><span style="font-size:20px;cursor:pointer" onclick="openNav()">&#9776; Profil Anda</span></strong></li>
   <li><a class="active" href="Homepage.php">Halaman Awal</a></li>
   <li><a href="profil_siswa.php">Data Anda</a></li>
   <li><a href="logout.php">Logout</a></li>
