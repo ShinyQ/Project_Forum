f128aa2bf49419290327c1c56af7bcfeff54601b
Kurniadi Ahmad Wijaya <kurniadiahmadwijaya@gmail.com>
2017-06-26 18:22:14 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,15 @@
-<<<<<<< HEAD
+
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Project Forum</title>
 	<meta name="viewport" content="width=device-width initial-scale=1 maximum-scale=1">
 </head>
-<body>
 
-=======
-<html> 
-<head> </head>
 <body> 
 <h1> Kurniadi Ahmad Wijaya </h1>
 <h1>Ahmad Hasib Haidar</h1>
 <h1>Diva Mujadidi</h1>
->>>>>>> 16998ce0aaeaa9ee555f289a181acfa535130973
+
 </body>
 </html>
