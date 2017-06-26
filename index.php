1e27c53a0f1072c08412a203c489bb997f7dfc4a
kurniadi <kurniadiahmadwijaya@gmail.com>
2017-06-26 18:20:54 +0700

    Add Meta Tag Responsive

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <html> 
-<head> </head>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
 <body> 
 <h1> Kurniadi Ahmad Wijaya </h1>
 <h1>Ahmad Hasib Haidar</h1>
