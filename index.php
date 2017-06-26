d0b0ce375dc7ccab9d1e97ff6fc13f36d49f7c1f
kurniadi <kurniadiahmadwijaya@gmail.com>
2017-06-26 19:19:45 +0700

    Hapus Elemen Latihan Sebelumnya

--- index.php
+++ index.php
@@ -7,15 +7,6 @@
 
 <body> 
 
-<header>
-	<span>Test Forum</span>
-</header>
-
-<h1>Kurniadi Ahmad Wijaya</h1>
-<h1>Ahmad Hasib Haidar</h1>
-<h1>Diva Mujaddidi</h1>
-
-<h1> Hello World </h1>
 
 </body>
 </html>
