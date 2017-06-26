5949d2995d0c4ffee584253249aa9d065e9bffee
kurniadi <kurniadiahmadwijaya@gmail.com>
2017-06-26 19:10:36 +0700

    Start Projek Hapus Elemen Tak Berguna

--- index.php
+++ index.php
@@ -1,20 +1,11 @@
 <html>
 <head>
-	<title>BlueForum</title>
+	<title>FORUM !!!</title>
 	<meta name="viewport" content="width=device-width initial-scale=1 maximum-scale=1">
 </head>
 
 <body> 
 
-<header>
-	<span>Blue Forum</span>
-</header>
-
-<h1>Kurniadi Ahmad Wijaya</h1>
-<h1>Ahmad Hasib Haidar</h1>
-<h1>Diva Mujadidi</h1>
-
-<h1> Hello World </h1>
 
 </body>
 </html>
