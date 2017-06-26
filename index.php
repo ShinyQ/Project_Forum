fdc5398273e0804dd4650cd2fdedc9fe32726c37
kurniadi <kurniadiahmadwijaya@gmail.com>
2017-06-26 20:03:31 +0700

    Tambah Header Di Index

--- index.php
+++ index.php
@@ -2,19 +2,45 @@
 <head>
 	<title>Test Forum</title>
 	<meta name="viewport" content="width=device-width initial-scale=1 maximum-scale=1">
+	<link href="assets/css/header.css" rel="stylesheet" type="text/css">
 </head>
 
 <body> 
 
-<header>
-	<span>Test Forum</span>
-</header>
+<div class="awal">
+<br>
+<strong>&nbsp;&nbsp;<!-- Untuk Image logo --></strong>
+<br>
 
-<h1>Kurniadi Ahmad Wijaya</h1>
-<h1>Ahmad Hasib Haidar</h1>
-<h1>Diva Mujaddidi</h1>
+<form class="form" style="float:right;">
 
-<h1> Hello World </h1>
+</form>
+<strong>
+<div style="float:center,padding-bottom:10px;">
+			
+			
+			<font size="6px" COLOR="#e52b2b">&nbsp; <!-- nama web --> </font>
+			<i>
+			<font color="#96b5e8" size="4px"><br>&nbsp;&nbsp;&nbsp;&nbsp;
+			<!-- slogan -->
+			</font>
+			</strong></i>
+
+</div>
+</div>
+<div class="navbar">
+<ul>
+<li>  
+<strong><span style="font-size:20px;cursor:pointer" onclick="openNav()">&#9776; Profil Anda</span></strong>
+</li>
+  <li><a class="active" href="Homepage.php">Halaman Awal</a></li>
+  <li><a href="profil_siswa.php">Data Anda</a></li>
+  <li><a href="logout.php">Logout</a></li>
+  <li><a href="prosses_hapus.php">Hapus Profil</a></li>
+  <li><a href="penulis.php">Tentang Pembuat Web</a></li>
+  <li><a href="">Tentang Website</a></li>
+</ul>
+</div>
 
 </body>
 </html>
