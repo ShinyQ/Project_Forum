8f17d270fbe094db5d1019be48da88fa007782e2
dididwizard <didid.wizard@gmail.com>
2017-06-26 19:45:05 +0700

    Hai Beb

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
 	<title>Test Forum</title>
 	<meta name="viewport" content="width=device-width initial-scale=1 maximum-scale=1">
+	<link rel="stylesheet" href="assets/css/header.css">
 </head>
 
 <body> 
