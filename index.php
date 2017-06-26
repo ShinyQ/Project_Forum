717ec4dd648eaa5e18054f221a643c50c07d4e63
dididwizard <didid.wizard@gmail.com>
2017-06-26 19:30:15 +0700

    File css dibuat

--- index.php
+++ index.php
@@ -10,11 +10,13 @@
 	<span>Test Forum</span>
 </header>
 
-<h1>Kurniadi Ahmad Wijaya</h1>
-<h1>Ahmad Hasib Haidar</h1>
-<h1>Diva Mujaddidi</h1>
+<main>
+	<h1>Kurniadi Ahmad Wijaya</h1>
+	<h1>Ahmad Hasib Haidar</h1>
+	<h1>Diva Mujaddidi</h1>
 
-<h1> Hello World </h1>
+	<h1> Hello World </h1>
+</main>
 
 </body>
 </html>
