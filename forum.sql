78b433e16124a48d89c9e75b1ba0b86afc922e97
kurniadi <kurniadiahmadwijaya@gmail.com>
2017-07-21 09:26:08 +0700

    Add Tabel User Yang Dapat Diakses Admin dan Perubahan Database Forum.sql sisi user

--- forum.sql
+++ forum.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 30, 2017 at 05:06 PM
+-- Generation Time: Jul 21, 2017 at 04:22 AM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 5.6.30
 
@@ -40,8 +40,8 @@ CREATE TABLE `profil_admin` (
 --
 
 INSERT INTO `profil_admin` (`id_admin`, `Nama_Admin`, `Email`, `Username`, `Password`, `Foto_Profil`) VALUES
-(11131424, 'Kurniadi Ahmad Wijaya', 'kurniadiahmadwijaya@gmail.com', 'ShinyQ', 'administhebest', ''),
-(11131425, '', '', 'admin', 'admin', '');
+(11131424, 'Kurniadi Ahmad Wijaya', 'kurniadiahmadwijaya@gmail.com', 'ShinyQ', 'administhebest', '20140145_1417228441696829_2389883581277949126_n.jpg'),
+(11131425, 'root', '', 'admin', 'admin', '');
 
 -- --------------------------------------------------------
 
@@ -50,19 +50,25 @@ INSERT INTO `profil_admin` (`id_admin`, `Nama_Admin`, `Email`, `Username`, `Pass
 --
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL DEFAULT '0',
-  `email` varchar(50) NOT NULL DEFAULT '0',
-  `password` varchar(50) NOT NULL DEFAULT '0',
-  `full_name` varchar(50) NOT NULL DEFAULT '0'
+  `id_user` int(11) NOT NULL,
+  `Nama_Lengkap` varchar(50) NOT NULL DEFAULT '0',
+  `Jenis_Kelamin` varchar(50) NOT NULL DEFAULT '0',
+  `Tanggal_Lahir` varchar(50) NOT NULL DEFAULT '0',
+  `Email` varchar(50) NOT NULL,
+  `Alamat` varchar(100) NOT NULL,
+  `Slogan` varchar(300) NOT NULL,
+  `Hobi_User` varchar(30) NOT NULL,
+  `Username` varchar(10) NOT NULL,
+  `Password` varchar(15) NOT NULL,
+  `Foto_Profil` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Data Pengguna';
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `email`, `password`, `full_name`) VALUES
-(1, 'admin', 'forumproject@localhost.com', 'admin', 'Admin');
+INSERT INTO `users` (`id_user`, `Nama_Lengkap`, `Jenis_Kelamin`, `Tanggal_Lahir`, `Email`, `Alamat`, `Slogan`, `Hobi_User`, `Username`, `Password`, `Foto_Profil`) VALUES
+(1, 'Kurniadi Ahmad Wijaya', 'L', '10-01-2002', 'kurniadiahmadwijaya@gmail.com', 'Jln Danau Ranau No 36', 'Nothing Is True Everything Permitted', 'Ngoding Web', 'ShinyQ', 'administhebest', '');
 
 --
 -- Indexes for dumped tables
@@ -78,7 +84,7 @@ ALTER TABLE `profil_admin`
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_user`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -93,7 +99,7 @@ ALTER TABLE `profil_admin`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
